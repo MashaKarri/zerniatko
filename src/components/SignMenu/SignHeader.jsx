0245c8d6d6bcfdfa0c432e49a08e6a85454275cc
danilT9 <daniltrotsko9@gmail.com>
2026-07-14 20:51:14 +0300

    upd2

--- src/components/SignMenu/SignHeader.jsx
+++ src/components/SignMenu/SignHeader.jsx
@@ -5,7 +5,7 @@ import logo from "../../assets/logo.svg";
 export default function SignHeader() {
   return (
     <header className={style.header}>
-      <NavLink to="/zerniatko">
+      <NavLink to="/zerniatko/">
         <img className={style.header__icon} src={logo} alt="Logo" />
       </NavLink>
     </header>
