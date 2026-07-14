bec9c2ee9b7b24eb3bea361ad60343b808ffd0e4
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2026-07-14 19:55:32 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-// import "./App.css";
-import { Route, Routes, NavLink } from "react-router-dom";
+
+import { Route, Routes, NavLink, Navigate } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import CategoriePage from "./pages/СategoriesPage/CategoriesPage";
 import ProductsPage from "./pages/ProductsPage/ProductsPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
-
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/categories" element={<CategoriePage />} />
-    
-      </Routes>
-            <ProductsPage />
-      <RegisterPage />
-    </>
-    <Routes>
       <Route path="/zerniatko" element={<MainPage />} />
       <Route path="/zerniatko/categories" element={<CategoriePage />} />
       <Route path="/zerniatko/products" element={<ProductsPage />} />
       <Route path="/zerniatko/register" element={<RegisterPage />} />
     </Routes>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,59 +3,45 @@ import logo from "../../assets/logo.svg";
 import button from "../../assets/button.svg";
 import style from "./Header.module.css";
 import { Container } from "../Container/ContainerLayout.jsx";
-import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
-
-  const setActiveClass = ({ isActive }) => 
-    isActive ? `${style.header__link} ${style.header__link_active}` : style.header__link;
-
   return (
     <header className={style.header}>
       <Container>
         <nav className={style.header__navigation}>
-
-
+          <NavLink to="/zerniatko" className={style.header__logo}>
             <img className={style.header__icon} src={logo} alt="Logo" />
-
-
+          </NavLink>
           <ul className={style.header__list}>
             <li className={style.header__item}>
-
-              <NavLink to="/" className={setActiveClass}>
+              <NavLink className={style.header__link} to="/zerniatko">
                 Головна
               </NavLink>
             </li>
             <li className={style.header__item}>
- 
-              <a className={style.header__link} href="#">
+              <NavLink className={style.header__link} to="/zerniatko/products">
                 Товари
               </NavLink>
             </li>
             <li className={style.header__item}>
-   
-              <NavLink to="/categories" className={setActiveClass}>
+              <NavLink className={style.header__link} to="/zerniatko/categories">
                 Категорії
               </NavLink>
             </li>
           </ul>
-
           <ul className={style.header__auth}>
             <li className={style.header__item}>
-            
-              <a className={style.header__link} href="#">
+              <NavLink className={style.header__link} to="/zerniatko/login">
                 Вхід
               </NavLink>
             </li>
             <li className={style.header__item}>
-              
-              <a className={style.header__link} href="#">
+              <NavLink className={style.header__link} to="/zerniatko/register">
                 Реєстрація
               </NavLink>
             </li>
           </ul>
         </nav>
-
         <button className={style.header__button}>
           <img className={style.header__basket} src={button} alt="корзина" />
         </button>
