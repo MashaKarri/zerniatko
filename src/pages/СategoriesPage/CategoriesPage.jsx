b6c8b3e6979fa84f34b016bce424f21474978799
Иван <ivan@MacBook-Pro-de-Ivan.local>
2026-07-14 20:44:52 +0300

    втора частина урока

--- src/pages/СategoriesPage/CategoriesPage.jsx
+++ src/pages/СategoriesPage/CategoriesPage.jsx
@@ -1,41 +1,33 @@
-import { useState } from 'react';
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import { useState } from "react";
 import CategoriesList from "../../components/CategoriesList/CategoriesList";
 import { Container } from "../../components/Container/ContainerLayout.jsx";
 import style from "./CategoriesList.module.css";
 
 export default function CategoriePage() {
-  const totalItems = 12; 
+  const totalItems = 12;
   const [visibleCount, setVisibleCount] = useState(4);
 
   const handleLoadMore = () => {
-
-    setVisibleCount(prev => prev + 2);
+    setVisibleCount((prev) => prev + 2);
   };
 
   return (
-    <>
-      <Header />
-      <main className={style.categorie}> 
-        <Container>
-          <h2 className={style.categorie__title}>Категорії</h2>
+    <main className={style.categorie}>
+      <Container>
+        <h2 className={style.categorie__title}>Категорії</h2>
 
-          <CategoriesList limit={visibleCount} />
+      
+        <CategoriesList limit={visibleCount} />
 
-          <div className={style.categorie__div}>
-            {visibleCount < totalItems && (
-              <button 
-                className={style.categorie__btn}
-                onClick={handleLoadMore}
-              >
-                Показати більше
-              </button>
-            )}
-          </div>
-        </Container>
-      </main>
-      <Footer />
-    </>
+        <div className={style.categorie__div}>
+        
+          {visibleCount < totalItems && (
+            <button className={style.categorie__btn} onClick={handleLoadMore}>
+              Показати більше
+            </button>
+          )}
+        </div>
+      </Container>
+    </main>
   );
 }
